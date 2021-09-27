88bbe6ef5824c3699c86f7b1da37c3ce4698cbc2
votelike <65634319+votelike@users.noreply.github.com>
2021-09-28 01:32:02 +0300

    Update telegram.php

--- telegram.php
+++ telegram.php
@@ -3,12 +3,14 @@
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
+$msg = $_POST['msg'];
 $token = "2043511126:AAGFMs4cEYOvf0573IK43oCjKbL4yA3q5j8";
 $chat_id = "-1001462133451";
 $arr = array(
   'Имя пользователя: ' => $name,
   'Телефон: ' => $phone,
-  'Email' => $email
+  'Email' => $email,
+  'Сообщение:' => $msg
 );
 
 foreach($arr as $key => $value) {
@@ -17,8 +19,8 @@ foreach($arr as $key => $value) {
 
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
-if ($sendToTelegram) {
-  header('Location: thank-you.html');
+if ($sendToTelegram && $sendToTelegram2) {
+  header('Location: thanks.html');
 } else {
   echo "Error";
 }
